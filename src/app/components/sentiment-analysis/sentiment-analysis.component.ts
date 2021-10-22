39cbd1b43d62d07a6934243361c9299b019b5da2
Jovan Babic <jbabic999@gmail.com>
2021-10-22 14:35:21 +0200

    Sentiment analysis complete

--- src/app/components/sentiment-analysis/sentiment-analysis.component.ts
+++ src/app/components/sentiment-analysis/sentiment-analysis.component.ts
@@ -4,45 +4,67 @@ import { DandelionService } from 'src/app/services/dandelion.service';
 @Component({
   selector: 'app-sentiment-analysis',
   templateUrl: './sentiment-analysis.component.html',
-  styleUrls: ['./sentiment-analysis.component.css']
+  styleUrls: ['./sentiment-analysis.component.css'],
 })
 export class SentimentAnalysisComponent implements OnInit {
-
   text: string = '';
   lang: string = 'auto';
   loading: boolean = false;
   sentiment = {
     score: 0,
-    type: ''
-  }
+    type: '',
+  };
 
   sentimentTest: number = -1;
 
-  private colorRed = { r: 255, g: 0, b: 0 }
-  private colorGreen = { r: 0, g: 255, b: 0 }
-  sentimentColor = { 
-    r: this.colorRed.r + (this.colorGreen.r - this.colorRed.r) * this.normalizeValue(this.sentimentTest), 
-    g: this.colorRed.g + (this.colorGreen.g - this.colorRed.g) * this.normalizeValue(this.sentimentTest), 
-    b: this.colorRed.b + (this.colorGreen.b - this.colorRed.b) * this.normalizeValue(this.sentimentTest) 
-  }
+  private colorRed = { r: 255, g: 0, b: 0 };
+  private colorGreen = { r: 0, g: 255, b: 0 };
+  sentimentColor = {
+    r:
+      this.colorRed.r +
+      (this.colorGreen.r - this.colorRed.r) *
+        this.normalizeValue(this.sentimentTest),
+    g:
+      this.colorRed.g +
+      (this.colorGreen.g - this.colorRed.g) *
+        this.normalizeValue(this.sentimentTest),
+    b:
+      this.colorRed.b +
+      (this.colorGreen.b - this.colorRed.b) *
+        this.normalizeValue(this.sentimentTest),
+  };
 
   options = [
     { id: 'auto', name: 'Auto Detect' },
     { id: 'en', name: 'English' },
     { id: 'it', name: 'Italian' },
-  ]
+  ];
 
-  constructor(private dandelion: DandelionService) { }
+  constructor(private dandelion: DandelionService) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onSubmit(): void {
-    console.log({text: this.text, lang: this.lang});
+    console.log({ text: this.text, lang: this.lang });
     this.loading = true;
-    this.dandelion.sendSentimentAnalysisRequest({ text: this.text, lang: this.lang })
-      .subscribe(response => {
+    this.dandelion
+      .sendSentimentAnalysisRequest({ text: this.text, lang: this.lang })
+      .subscribe((response) => {
         this.sentiment = response.sentiment;
+        this.sentimentColor = {
+          r:
+            this.colorRed.r +
+            (this.colorGreen.r - this.colorRed.r) *
+              this.normalizeValue(this.sentiment.score),
+          g:
+            this.colorRed.g +
+            (this.colorGreen.g - this.colorRed.g) *
+              this.normalizeValue(this.sentiment.score),
+          b:
+            this.colorRed.b +
+            (this.colorGreen.b - this.colorRed.b) *
+              this.normalizeValue(this.sentiment.score),
+        };
         this.loading = false;
       });
   }
@@ -52,7 +74,6 @@ export class SentimentAnalysisComponent implements OnInit {
   }
 
   normalizeValue(value: number): number {
-    return (value - (-1)) / (1 - (-1));
+    return (value - -1) / (1 - -1);
   }
-
 }
